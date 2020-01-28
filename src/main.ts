feb7426cd8cac72d8b2927688761157b1ebd0a99
Victor <utrera1695@gmail.com>
2020-01-28 15:33:52 -0400

    global pewfix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'; /*
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api2');
   app.enableCors();
   /* const options = new DocumentBuilder()
     .setTitle('API SALON')
