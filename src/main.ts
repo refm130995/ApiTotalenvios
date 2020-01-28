9190811cb9b60b5dfd8561e298c4789ca5e4fa95
Victor <utrera1695@gmail.com>
2020-01-28 12:52:24 -0400

    swagger delete

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module'; /* 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const options = new DocumentBuilder()
+  /* const options = new DocumentBuilder()
     .setTitle('API SALON')
     .setDescription('Esta es una API dedicada a la aplicacion mobile de barberias')
     .setVersion('1.0')
@@ -13,7 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   
   SwaggerModule.setup('', app, document);
-
-   await app.listen(8500);
+ */
+  await app.listen(8500);
 }
 bootstrap();
